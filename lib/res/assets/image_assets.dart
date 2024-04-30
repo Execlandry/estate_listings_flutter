b9341ae6fa16921715df61e2ce1511e638e5522d
execlandry <nikhilkerkarnk21@gmail.com>
2024-04-30 20:52:41 +0530

    initial commit

---
+++ lib/res/assets/image_assets.dart
@@ -0,0 +1,3 @@
+class ImageAssets {
+  static const String SplashView = "assets/images/splash_screen.png";
+}
---
+++ lib/res/routes/routes.dart
@@ -0,0 +1,23 @@
+// import 'package:get/get.dart';
+
+
+// class AppRoutes {
+//   static appRoutes() => [
+//         GetPage(
+//             name: RouteName.splashView,
+//             page: () =>  SplashView(),
+//             transition: Transition.leftToRightWithFade,
+//             transitionDuration: const Duration(milliseconds: 250)),
+//         GetPage(
+//           name: RouteName.loginView, 
+//           page: () =>  LoginView(),
+//           transition: Transition.leftToRightWithFade,
+//           transitionDuration: const Duration(milliseconds: 250)),
+
+//           GetPage(
+//           name: RouteName.homeView, 
+//           page: () =>  HomeView(),
+//           transition: Transition.leftToRightWithFade,
+//           transitionDuration: const Duration(milliseconds: 250)),
+//       ];
+// }
